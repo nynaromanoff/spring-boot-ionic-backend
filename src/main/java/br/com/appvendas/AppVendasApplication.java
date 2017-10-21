e93dc6f9b0749c7e50fa1d9c1be490f26dc744ad
Jessica Santos <jel_008@hotmail.com>
2017-10-21 19:53:46 -0200

    Projeto Criado

---
+++ src/main/java/br/com/appvendas/AppVendasApplication.java
@@ -0,0 +1,12 @@
+package br.com.appvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppVendasApplication.class, args);
+	}
+}
