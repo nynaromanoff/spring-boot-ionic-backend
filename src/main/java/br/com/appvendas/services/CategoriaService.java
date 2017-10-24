2fb56923430d9d2c1c971067d2a20ab541aba60e
Jessica Santos <jel_008@hotmail.com>
2017-10-24 10:56:28 -0200

    Ajuste nos endpoint /categorias/{id}

--- src/main/java/br/com/appvendas/services/CategoriaService.java
+++ src/main/java/br/com/appvendas/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.appvendas.domain.Categoria;
 import br.com.appvendas.repositories.CategoriaRepository;
+import br.com.appvendas.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -15,7 +16,10 @@ public class CategoriaService {
 	public Categoria buscar(Integer id){
 		
 		Categoria obj = repo.findOne(id);
-		
+		if(obj == null){
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: "+ id
+					+", Tipo: "+Categoria.class.getName());
+		}
 		return obj;
 		
 	}
