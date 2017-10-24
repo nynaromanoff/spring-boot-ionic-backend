5272010a1d67a76798dc0a1f2467dc5c0ada51a5
Jessica Santos <jel_008@hotmail.com>
2017-10-24 13:21:12 -0200

    Endpoint/cliente/{id} disponível

---
+++ src/main/java/br/com/appvendas/services/ClienteService.java
@@ -0,0 +1,26 @@
+package br.com.appvendas.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.appvendas.domain.Cliente;
+import br.com.appvendas.repositories.ClienteRepository;
+import br.com.appvendas.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id){
+		
+		Cliente obj = repo.findOne(id);
+		if(obj == null){
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: "+ id
+					+", Tipo: "+Cliente.class.getName());
+		}
+		return obj;
+		
+	}
+}
