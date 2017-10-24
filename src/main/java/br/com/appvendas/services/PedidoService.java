53b3b65973d960024e086fef78b159cf31a59f6b
Jessica Santos <jel_008@hotmail.com>
2017-10-24 17:03:00 -0200

    Endpoint /pedidos/{id} Disponibilizado

---
+++ src/main/java/br/com/appvendas/services/PedidoService.java
@@ -0,0 +1,27 @@
+package br.com.appvendas.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.appvendas.domain.Pedido;
+import br.com.appvendas.repositories.PedidoRepository;
+import br.com.appvendas.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id){
+		
+		Pedido obj = repo.findOne(id);
+		if(obj == null){
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: "+ id
+					+", Tipo: "+Pedido.class.getName());
+		}
+		
+		return obj;
+		
+	}
+}
