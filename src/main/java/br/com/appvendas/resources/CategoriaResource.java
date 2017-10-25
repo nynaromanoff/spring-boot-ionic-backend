8cd5b2135b58c7c027a4d445650271d60c2b9e9b
Jessica Santos <jel_008@hotmail.com>
2017-10-25 10:53:03 -0200

    Atualizando Categoria com PUT

--- src/main/java/br/com/appvendas/resources/CategoriaResource.java
+++ src/main/java/br/com/appvendas/resources/CategoriaResource.java
@@ -26,8 +26,8 @@ public class CategoriaResource {
 	private CategoriaService service;
 	
 	@RequestMapping(value= "/{id}",method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id){
-			Categoria obj = service.buscar(id);
+	public ResponseEntity<Categoria> find(@PathVariable Integer id){
+			Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	
 		
@@ -45,6 +45,13 @@ public class CategoriaResource {
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 		*/
+	}
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT, consumes=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj,@PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		
+		return ResponseEntity.noContent().build();
 	}
 
 }
