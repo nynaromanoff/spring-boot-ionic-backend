3360bf6edb8b5587d698ac81705f9dde57034196
Jessica Santos <jel_008@hotmail.com>
2018-04-15 15:43:44 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/br/com/appvendas/AppVendasApplication.java
+++ src/main/java/br/com/appvendas/AppVendasApplication.java
@@ -2,14 +2,19 @@ package br.com.appvendas;
 
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.com.appvendas.services.S3Service;
 
 
 @SpringBootApplication
 public class AppVendasApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
 
 	
 	public static void main(String[] args) {
@@ -19,6 +24,7 @@ public class AppVendasApplication implements CommandLineRunner {
 	@Override
 	public void run(String... arg0) throws Exception {
 		
+		s3Service.uploadFile("D:\\AppVendas\\fotos\\lua.jpg");
 	}
 	
 	
