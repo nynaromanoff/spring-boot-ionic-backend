ac11e0be8fcaa69613937306e9b39ef6f4b54752
Jessica Santos <jel_008@hotmail.com>
2018-09-19 21:08:11 -0300

    Atualização de endereço do Heroku

--- src/main/java/br/com/appvendas/resources/PedidoResource.java
+++ src/main/java/br/com/appvendas/resources/PedidoResource.java
@@ -7,7 +7,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
